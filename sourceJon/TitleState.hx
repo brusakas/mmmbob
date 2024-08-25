ff39c68865a2b2bc2520c8c314f0260357877844
brusakas <121342424+brusakas@users.noreply.github.com>
2024-08-25 12:38:44 +0100

    fuck off ngio :angry:

--- sourceJon/TitleState.hx
+++ sourceJon/TitleState.hx
@@ -21,7 +21,7 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
-import io.newgrounds.NG;
+//import io.newgrounds.NG;
 import lime.app.Application;
 import openfl.Assets;
 
@@ -80,10 +80,11 @@ class TitleState extends MusicBeatState
 
 		// NGio.noLogin(APIStuff.API);
 
-		#if ng
+		/*#if ng
 		var ng:NGio = new NGio(APIStuff.API, APIStuff.EncKey);
 		trace('NEWGROUNDS LOL');
 		#end
+                */
 
 		FlxG.save.bind('funkin', 'ninjamuffin99');
 
@@ -286,14 +287,15 @@ class TitleState extends MusicBeatState
 
 		if (pressedEnter && !transitioning && skippedIntro)
 		{
-			#if !switch
+			/*#if !switch
 			NGio.unlockMedal(60960);
 
 			// If it's Friday according to da clock
 			if (Date.now().getDay() == 5)
 				NGio.unlockMedal(61034);
 			#end
-
+                        */
+			
 			titleText.animation.play('press');
 
 			FlxG.camera.flash(FlxColor.WHITE, 1);
